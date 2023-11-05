cd0ff313cc4b22fdfdef5936d5a5fe8838926cb6
manickam786 <73480675+manickam786@users.noreply.github.com>
2023-11-05 22:03:53 +0530

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $to = $_POST["to"];
     $subject = $_POST["subject"];
     $message = $_POST["message"];
-    $headers = "From: your_email@example.com"; // Replace with your email address
+    $headers = "From: m.v.manickam@gmail.com"; // Replace with your email address
 
     if (mail($to, $subject, $message, $headers)) {
         echo "Email sent successfully.";
